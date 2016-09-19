2b054c330f6c30b5bb06654b94424657ceef7d59
Nick Rigano <nrigano@Executor.local>
2016-09-19 15:49:40 -0400

    Complete

--- Help.playground/Contents.swift
+++ Help.playground/Contents.swift
@@ -4,3 +4,5 @@ name.uppercased()
 let uppercaseName = name.uppercased()
 print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
 // prints "Audrey... Audrey... AUDREY! WAKE UP!")
+
+name.customMirror()
